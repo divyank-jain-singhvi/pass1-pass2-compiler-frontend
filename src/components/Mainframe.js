96da0b54894e1a852dadd56cb9e7782bc0a3cdf1
Divyank Jain Singhvi <119522879+divyank-jain-singhvi@users.noreply.github.com>
2024-04-01 01:14:33 +0530

    created frontend in one file added requesting and sending data

--- src/components/Mainframe.js
+++ src/components/Mainframe.js
@@ -1,50 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import './Mainframe.css'
+
+import './mainframe.css'
+import React, { useState } from 'react'
 import axios from 'axios';
-import Inputcode from './Inputcode';
-
+import { useNavigate } from 'react-router-dom'
 export default function Mainframe() {
-// for fetch data from api
-  const [outputs,setoutputs]=useState([]);
-  const [result,setresult]=useState("");
 
-  const getoutput = async () =>{
-    const response=await axios.get('http://localhost:8000/api/')
-    console.log(response.data)
-    setoutputs(response.data)
-    // console.log(output)
+    const[codes,setcodes]=useState("");
+    const[outputs,setoutputs]=useState("Terminal");
+    let history = useNavigate();
+    const SendCode = async () => {
+    let codeBlock = {
+        'code': codes,
+        'output': 'Terminal',
+    };
+        await axios({
+            method: 'post',
+            url: 'http://localhost:8000/api/',
+            data: codeBlock,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            console.log(response.data);
+            history.push('/');
+        }).catch((error) => {
+            console.error("Error:", error);
+        });
+        const response=await axios.get('http://localhost:8000/api/')
+        console.log(response.data)
+        setoutputs(response.data)
+    };
 
-  }
 
-  useEffect(()=>{
-    getoutput();
-  })
-
+    // const getoutput = async () =>{
+    //     const response=await axios.get('http://localhost:8000/api/')
+    //     setoutputs(response.data)
+    //   }
+    //   useEffect(()=>{
+    //     getoutput();
+    //   })
 
 
   return (
     <>
-    <div className='d-flex lables'>
-      <div className='editor'>
-     </div>
-     <div className='d-flex lables'>
-        
-    </div>
-    </div>
-    <div className='d-flex justify-content-between m-auto blocks'>
-      <div className='d-flex'>
-       
-      {/* <textarea className='code-block my-3 mx-3 text-white'></textarea> */}
-      <Inputcode/>
-      <div className='font'>
-      <label htmlFor="">Terminal</label>
-      <textarea className='code-output my-3 text-white' value={outputs[Object.keys(outputs).length-1].output} readOnly></textarea>
-      </div>
-        {/* outputs[ Object.keys(outputs).length-1].output */}
-      </div>
+        <div className="d-flex">
         <div className='info ml-2 bg-dark'></div>
-    </div>
-        
+        <div className='p-2'>
+                <div className="mx-2 py-2">
+                <button type="button" className="btn btn-success run" onClick={SendCode}>Run</button>
+                </div>
+                <div className="d-flex">
+                <textarea className="bg-dark mx-2 text-white rounded output px-3 py-3" name="code" onChange={(e)=>setcodes(e.target.value)} placeholder='' value={codes} required></textarea>
+                <textarea className="bg-dark mx-2 text-white rounded output px-3 py-3" name="output" value={outputs[Object.keys(outputs).length-1].output} readOnly></textarea>
+                </div>
+        </div>
+      </div>
     </>
   )
 }
